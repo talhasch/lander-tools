135828eb8594f04002fcc1ec7281c2673e747445
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-01-10 14:44:44 +0300

    resize support

--- src/lander/image_pipe/app.py
+++ src/lander/image_pipe/app.py
@@ -1,9 +1,11 @@
 import base64
 import os
 import tempfile
+from io import BytesIO
 
 import magic
 import requests
+from PIL import Image
 from flask import Flask, make_response, abort
 from flask_caching import Cache
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -22,7 +24,7 @@ def __flask_setup():
     cache_config = {'CACHE_TYPE': 'filesystem',
                     'CACHE_THRESHOLD': 10000,
                     'CACHE_DEFAULT_TIMEOUT': 86400,
-                    'CACHE_DIR': os.path.join(tempfile.gettempdir(), 'image_pipe')}
+                    'CACHE_DIR': os.path.join(tempfile.gettempdir(), 'i-p')}
     cache = Cache(with_jinja2_ext=False, config=cache_config)
     cache.init_app(app)
 
@@ -31,9 +33,21 @@ def __flask_setup():
         return 'HELLO'
 
     @app.route('/i-p/<code>')
-    def serve(code):
+    @app.route('/i-p/<code>/<size>')
+    def serve(code, size=None):
 
-        rv = cache.get(code)
+        cache_key = code
+
+        if size is not None:
+            try:
+                width, height = [int(x) for x in size.split('x')]
+                assert 20 <= width <= 1000
+                assert 20 <= height <= 1000
+                cache_key = '{}/{}/{}'.format(cache_key, width, height)
+            except BaseException:
+                abort(406)
+
+        rv = cache.get(cache_key)
 
         try:
             url = base64.b64decode(code).decode('utf-8')
@@ -48,7 +62,16 @@ def __flask_setup():
         if rv is None:
             resp = requests.get(url)
             rv = resp.content
-            cache.set(code, rv)
+
+            if size:
+                im_io = BytesIO()
+                im = Image.open(BytesIO(rv))
+                im.thumbnail((width, height))
+                im.save(im_io, 'JPEG', quality=96)
+                im_io.seek(0)
+                rv = im_io.read()
+
+            cache.set(cache_key, rv)
 
         response = make_response(rv)
         response.set_etag(code)
